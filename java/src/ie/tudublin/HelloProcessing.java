f47b3c30e0e596511b86b3783dcea5aac182969b
scalar97 <45573453+scalar97@users.noreply.github.com>
2020-02-03 12:52:40 +0000

    week 2 code

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -2,7 +2,7 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class HelloProcessing extends PApplet
+public class HelloProcessing extends PApplet /*PApplet is a supper class that HelloProcessing of HP*/
 {	
 	public void settings()
 	{
@@ -30,11 +30,41 @@ public class HelloProcessing extends PApplet
 	
 	public void draw()
 	{	
+		background(69,0,0);
+		//oulinte
+		stroke(0,0,255);
+
+
+		line(10, 10, 100, 100); // x1, y1, x2 ,y2
+
+		point(50,50); //x, y
+
+		noStroke();
 		
+		fill(0,0,255);
+		circle(250, 290, 390);
+	
+		//rect(70, 150, 90, 10); //tlx, tly, w, h
+
+		fill(0,255,255);
+		triangle(250, 50, 50, 450, 450, 450); //
+
+		fill(69,0,0);
+		ellipse(250, 250, 200, 100); // cx, cy, w,h
+
+		fill(0,0,0);
+		circle(250, 250, 50);
+
+	
+
+
+
+		/*
 		background(0);
 		noStroke();
 		fill(255);
 		ellipse(x, mouseY, 50, 50);		
 		x ++;
+		*/
 	}
 }
