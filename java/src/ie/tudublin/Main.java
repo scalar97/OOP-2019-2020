56c48db0686d3eb93eccfabf1d975b2b73495cf8
scalar97 <45573453+scalar97@users.noreply.github.com>
2020-02-10 12:35:57 +0000

    try to avoid merge conflict

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -3,24 +3,47 @@ package ie.tudublin;
 public class Main
 {
 
-    public void helloProcessing()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing());
-	}
+  
     public static void main(String[] arg)
     {
+        
         System.out.println("Hello world!");
 
-        Animal misty = new Dog(); // Polymorphism
-        misty.setName("Misty");
-        System.out.println(misty.getName());  
-        misty.speak();  
-        Cat topcat = new Cat();
-        topcat.speak();
+        /* What is polymorpism?
+            The type is of a Superclass, the instance is a subclass*/
 
-        Main main = new Main();
-		main.helloProcessing();
+        // This is not polymorphism    
+        Cat topcat = new Cat("Topcat");
+
+        //This polymorphism the type is of super class Animal & Instance is of subclass Cat
+        Animal mino = new Cat("Mino");
+
+        //virtual method also know as dynamic binding 
+        //this prints out the cat method speak meow not the Animal Method
+        mino.speak();
+
+        /* This wont compile 
+         System.out.println(mino.getNumLives());*/
+
+        //This will as we are now casting 
+        System.out.println((((Cat)mino).getNumLives()));
+
+        //This will compile
+        mino = new Dog("Misty");
+
+        Animal a = new Cat("a");
+        Animal b  = new Cat("b");
+
+        System.out.println(a);
+        System.out.println(b);
+
+        a = b;
         
+        a.setName("c");
+
+        System.out.println(a);
+        System.out.println(b);
+
+
     }
 }
